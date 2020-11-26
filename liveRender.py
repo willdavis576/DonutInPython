251a299920f789c5fcd9d5d52597937de18d82dd
Will Davis <willdavis576@gmail.com>
2020-11-26 21:47:27 +0000

    stupid error with x1 and x

--- liveRender.py
+++ liveRender.py
@@ -19,7 +19,7 @@ def findClosest(K1, K2, ang, yAng):
     x,y,z,L = genPoints(1,2, radians(ang), radians(yAng), 2, 2)
     x1 = K1 * x / (K2 + z)
     y1 = K1 * y / (K2 + z)
-    return x, y, L
+    return x1, y1, L
 
 def map(raw, myMax, myMin, targMax, targMin):
     return ((targMax - targMin) / (myMax - myMin)) * (raw + 1.5)
@@ -35,8 +35,8 @@ for i in range(36):
     for j in range(36):
         x, y, L = findClosest(K1, K2, 10*i, 10*j)
         turt.pu()
-        turt.setx(x*50)
-        turt.sety(y*50)
+        turt.setx(x*20)
+        turt.sety(y*20)
         size = map(L,3,0,7,0) + 1
         turt.dot(size)
         print(i,j)
