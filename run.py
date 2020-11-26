ad69c637ca23037ad23bc2d7589467d0adeeb228
Will Davis <willdavis576@gmail.com>
2020-11-26 15:33:04 +0000

    added Luminance to the plotly graph

--- run.py
+++ run.py
@@ -6,26 +6,31 @@ import plotly.graph_objects as go
 
 
 def genPoints(R1, R2, ang, yAng, A, B):
+    #3D Torus
     x = (R2 + R1*cos(ang))*(cos(B)*cos(yAng) + sin(A)*sin(B)*sin(yAng)) - R1*cos(A)*sin(B)*sin(ang)
     y = (R2 + R1*cos(ang))*(cos(yAng)*sin(B) - cos(B)*sin(A)*sin(yAng)) + R1*cos(A)*cos(B)*sin(ang)
     z = cos(A)*(R2 + R1*cos(ang))*sin(yAng) + R1*sin(A)*sin(ang)
-    return x,y,z
+    #Luminance
+    L = cos(yAng)*cos(ang)*sin(B) - cos(A)*cos(ang)*sin(yAng) - sin(A)*sin(ang)+cos(B)*(cos(A)*sin(ang) - cos(ang)*sin(A)*sin(yAng))
+    return x,y,z,L
 
 xList = np.array([])
 yList = np.array([])
 zList = np.array([])
+LList = np.array([])
 
-for i in range(0, 60):
-    for j in range(0,60):
-        x,y,z = genPoints(1,2, radians(6*i), radians(6*j), 1,1) #get torus coords, 0 -> 2pi. using 120 and scaling to have less processing power
+for i in range(0, 120):
+    for j in range(0,120):
+        x,y,z,L = genPoints(1,2, radians(3*i), radians(3*j), 1,1) #get torus coords, 0 -> 2pi. using 120 and scaling to have less processing power
         xList = np.append(xList, x)
         yList = np.append(yList, y)
         zList = np.append(zList, z)
+        LList = np.append(LList, L)
 
 
-data = [go.Scatter3d(x=xList, y=yList, z=zList,mode='markers', marker=dict(size=1))]
+data = [go.Scatter3d(x=xList, y=yList, z=zList,mode='markers', marker=dict(size=1, color=LList))]
 fig = go.Figure(data=data)
-# fig.show()
+fig.show()
 
 
 #find pixels closest to screen
@@ -46,18 +51,31 @@ fig = px.scatter(x=xView, y=yView) #2D Drawing
 # fig.show()
 
 
-xTurt = turtle.Turtle()
-xTurt.speed(10)
-
-def dot(Turt, x, y):
-    Turt.pu()
-    Turt.setx(x)
-    Turt.sety(y)
-    Turt.dot(5)
 
 
-for i in range(len(xView)):
-    dot(xTurt, xView[i]*50, yView[i]*50)
+
+
+
+
+'''
+TURTLEEEEEEEEEE
+'''
+#
+# xTurt = turtle.Turtle()
+# xTurt.speed(10)
+#
+# def dot(Turt, x, y):
+#     Turt.pu()
+#     Turt.setx(x)
+#     Turt.sety(y)
+#     Turt.dot(5)
+#
+#
+# for i in range(len(xView)):
+#     dot(xTurt, xView[i]*50, yView[i]*50)
+
+
+
 
 
 
